38524d0bcb218d3c2c6c41535c6b60cf860f249e
CoderFrede1010 <frfu0001@stud.kea.dk>
2024-11-25 22:32:20 +0100

    Oprettelse af readFile() og writeToFile() i DatabseHandler
    en toCSV metode i Member class

--- src/DatabaseHandler.java
+++ src/DatabaseHandler.java
@@ -1,6 +1,47 @@
+import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DatabaseHandler {
-private ArrayList<Member> listOfMemebers;
-//Filreader() csvDatabseReader = new Filereader;
+
+    private static String path = "src//Database.csv";
+    private static String line = "";
+
+    public static void readFile() { //Metode til at læse hele Databse.csv filen
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                System.out.println(Arrays.toString(values));
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void writeToFile(Member member) { // Metode til at tilføje et Member til Database.csv filen
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(path, true));
+            bw.write(member.toCSV());
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        readFile(); //Eksempel på brug uden brugerinput
+
+        Member newMember = new Member("Ben Dover", LocalDate.of(1990, 5, 20));
+        writeToFile(newMember);
+
+        readFile();
+
+    }
 }
