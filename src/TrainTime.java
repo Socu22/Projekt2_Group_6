5a28a806d9441e7a560f5485f3afa17861268e74
KEAmimi <mimi0001@stud.kea.dk>
2024-11-28 10:25:43 +0100

    Small changes in userInterface

--- src/TrainTime.java
+++ src/TrainTime.java
@@ -2,18 +2,20 @@ import java.text.DecimalFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 
-public class TrainTime {
+public class TrainTime implements Comparable<TrainTime>{
     private String modtagetMemberName;
     private int modtagetMemberID;
     private LocalDate date;         //Remember which day the time was set
     private double duration;        //How fast was the trainging
     private String discipline;      //The swimming discipline
     private int distance;           //The Distance swam
+    private double performance;     //The ratio of Time to distance to measure performance
     private String secOrMin = " sekunder";
     DecimalFormat df = new DecimalFormat("#.00");
 
     public TrainTime(Member member, LocalDate date, double timeSec, String discipline, int distance){
         this.date = date;
+        this.performance = distance/timeSec;
         this.duration = setDuration(timeSec);
         this.discipline = discipline.toUpperCase();
         this.distance = distance;
@@ -43,6 +45,10 @@ public class TrainTime {
         return distance;
     }
 
+    public double getPerformance(){
+        return performance;
+    }
+
     public LocalDate getDate() {
         return date;
     }
@@ -52,7 +58,17 @@ public class TrainTime {
     }
 
     public static void main(String[] args) {
-       // TrainTime t1 = new TrainTime(LocalDate.now(), 80.4, "Crawl", 1000);
-       // System.out.println(t1);
+        Member m1 = new Member("Rasmus", LocalDate.of(1997, 03, 14));
+        TrainTime t1 = new TrainTime(m1, LocalDate.now(), 60, "Crawl", 1000);
+        System.out.println(t1.performance);
+    }
+
+    @Override
+    public int compareTo(TrainTime o){
+        if(this.performance<o.performance)
+            return -1;
+        else if(o.performance<this.performance)
+            return 1;
+        return 0;
     }
 }
