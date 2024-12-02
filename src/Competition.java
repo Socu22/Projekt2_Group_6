5eb354e45ed4eb26376a5a2379518134081d18de
JuliusGissel <Julius.gissel@gmail.com>
2024-12-02 11:02:59 +0100

    competition class show+add competitionTime

--- src/Competition.java
+++ src/Competition.java
@@ -7,8 +7,23 @@ public class Competition {
     int placement; // i forhold til andre
     LocalDate date;
 
+    public Competition(String name, String disciplin, int placement, LocalDate date) {
+        this.name = name;
+        this.disciplin = disciplin;
+        this.placement = placement;
+        this.date = date;
+    }
 
-    public void addCompetitionTime() {
+    public void addCompetitionTime(String time) {
+        this.timeResult = time;
+    }
 
+    public void showCompetitionTime() {
+        if (timeResult == null) {
+            System.out.println("Der er ingen tid for denne disciplin");
+        } else {
+            System.out.println("Tid for: " + name + "i disciplinen: " + disciplin + ": " + timeResult);
+        }
     }
 }
+
