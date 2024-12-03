649634e9bebc7a567b576bc4d5fea61eec096b17
oahlers <oliv2527@stud.kea.dk>
2024-12-03 08:44:55 +0100

    deleteCurrentMember fixed + added savefunktion (in progress)

--- src/AdminInterface.java
+++ src/AdminInterface.java
@@ -1,56 +1,60 @@
+import javax.xml.crypto.Data;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class AdminInterface {
-    public static int input=-1;
+    public static int input = -1;
     public static String inputString;
     private Member currentMember;
-    public static boolean running= true;
+    public static boolean running = true;
 
 
-    public void start_interface_Admin(){
+    public void start_interface_Admin() {
         while (running) {
 
             System.out.println("Tast 1 find specifikt medlem: ");
             System.out.println("Tast 2 for at registerer nyt medlem: ");
-            System.out.println("Tast 3 for at tilføje gæld til medlem: ");
-            System.out.println("Tast 4 for at registerer betalt gæld for medlem: ");
-            System.out.println("Tast 5 for finde top 5 i alle disicpilner : ");
-            System.out.println("Tast 6 for at se medlemmer gæld (IKKE UNDERSTØTTET ENDNU)");
-            System.out.println("Tast 7 for at gå tilbage");
-            input= InputHandler.inputInt("Vælg et af følgende valg: ");
+            System.out.println("Tast 3 for at fjerne et medlem");
+            System.out.println("Tast 4 for at tilføje gæld til medlem: ");
+            System.out.println("Tast 5 for at registerer betalt gæld for medlem: ");
+            System.out.println("Tast 6 for finde top 5 i alle discipliner : ");
+            System.out.println("Tast 7 for at se medlemmer gæld (IKKE UNDERSTØTTET ENDNU)");
+            System.out.println("Tast 8 for at gå tilbage");
+            input = InputHandler.inputInt("Vælg et af følgende valg: ");
 
 
             switch (input) {
                 case 1:
                     //get current member
                     setCurrentMember();
-                    System.out.println("Du er nu logget på medlemmet: "+currentMember);
+                    System.out.println("Du er nu logget på medlemmet: " + currentMember);
                     break;
                 case 2:
                     //create Member
                     createMember();
                     break;
                 case 3:
+                    //deletes member
+                    deleteCurrentMember();
+                    break;
+                case 4:
                     //Add debt to Member  object-
                     addDebtToMember();
                     break;
-                case 4:
+                case 5:
                     //pay debt for Member object
                     payDebtForMember();
                     break;
-                case 5:
+                case 6:
                     topFiveForEachDiscipline();
                     break;
-                case 6:
-                   printAllNonPaidMembers();
-                    break;
                 case 7:
-                    back();
+                    printAllNonPaidMembers();
                     break;
                 case 8:
-                    deleteCurrentMember();
+                    back();
+                    break;
             }
         }
     }
@@ -61,7 +65,7 @@ public class AdminInterface {
 
     public void setCurrentMember() {
         try {
-            currentMember= MemberHandler.smartSearch();
+            currentMember = MemberHandler.smartSearch();
             System.out.println(currentMember);
         } catch (MemberNotFoundException e) {
             System.out.println(e.toString());
@@ -70,20 +74,20 @@ public class AdminInterface {
         }
     }
 
-    public Member getCurrentMember(){
+    public Member getCurrentMember() {
         return currentMember;
     }
 
-    public void createMember(){
-        try{
+    public void createMember() {
+        try {
             MemberHandler.createMember();
             currentMember = MemberHandler.getNewestMember();
             System.out.println("Genvej(tast): betal gæld(1), quit(2) ");
-            input=InputHandler.inputInt("Vælg et af følgende valg: ");
+            input = InputHandler.inputInt("Vælg et af følgende valg: ");
         } catch (InvalidInputException e) {
             System.out.println(e.getMessage());
         }
-        switch (input){
+        switch (input) {
             case 1:
                 currentMember.payDebt();
                 break;
@@ -93,8 +97,8 @@ public class AdminInterface {
     }
 
 
-    public void deleteCurrentMember(){
-        if (currentMember==null){
+    public void deleteCurrentMember() {
+        if (currentMember == null) {
             System.out.println("Du skal først søge efter medlem, før du kan slette.");
             try {
                 setCurrentMember(); // Prompt the user to search for a member
@@ -104,27 +108,31 @@ public class AdminInterface {
             }
         }
 
-        System.out.println("Vil du slette medlem: " +getCurrentMember().getName());
-        System.out.println("Skriv 'Ja' hvis du vil slette " +getCurrentMember().getName());
+        System.out.println("Vil du slette medlem: " + getCurrentMember().getName());
+        System.out.println("Skriv 'Ja' hvis du vil slette " + getCurrentMember().getName());
         System.out.println("og 'Nej' hvis du vil gå tilbage");
-        inputString=InputHandler.inputString("indtast valg");
-        switch (inputString){
+        inputString = InputHandler.inputString("indtast valg");
+        switch (inputString) {
             case "Ja":
-                try{
+                try {
                     boolean removed = MemberHandler.getMemberList().removeIf(member -> member.equals(currentMember));
                     if (removed) {
-                        System.out.println("Medlemmet " +currentMember.getName()+ "er blevet slettet.");
-                        currentMember=null;
+                        System.out.println("Medlemmet " + currentMember.getName() + " er blevet slettet.");
+                        currentMember = null;
+                        DatabaseHandler.save();
+
                     } else System.out.println("Medlemmet kunne ikke findes på listen");
                 } catch (Exception e) {
                     System.out.println("der opstod en fejl under sletning af ");
                 }
-                back();
-                    break;
+                break;
             case "Nej":
+                System.out.println("Sletning af bruger annulleret.");
                 break;
         }
+        back();
     }
+
 
     public void addDebtToMember(){
 
@@ -145,18 +153,13 @@ public class AdminInterface {
     public void topFiveForEachDiscipline(){
        TimeSort.topFiveForEachDiscipline();
     }
-   public void printAllNonPaidMembers(){
-        List<Member> loaded= MemberHandler.loadFromDatabase();
-       for (int i = 0; i < loaded.size() ; i++) {
-           if(loaded.get(i).getDebt()>0){
+   public void printAllNonPaidMembers() {
+       List<Member> loaded = MemberHandler.loadFromDatabase();
+       for (int i = 0; i < loaded.size(); i++) {
+           if (loaded.get(i).getDebt() > 0) {
                System.out.println(loaded.get(i).toString());
            }
 
        }
-    }
-
-
-
-
-
+   }
 }
